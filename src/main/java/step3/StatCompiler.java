6ff2cf0d245ffdd2e9649ad7f4bbadd893dacd06
sk1737030 <sk1737030@naver.com>
2021-07-15 23:37:47 +0900

    2부 1장 First Fast Isolated Repeatable Self validating Timely

---
+++ src/main/java/step3/StatCompiler.java
@@ -0,0 +1,48 @@
+package step3;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StatCompiler {
+    private QuestionController controller = new QuestionController();
+
+    public Map<String, Map<Boolean, AtomicInteger>> responseByQuestion(List<BooleanAnswer> answers) {
+        Map<String, Map<Boolean, AtomicInteger>> responses = new HashMap<>();
+        answers.stream().forEach(answer -> incrementHistogram(responses,answers));
+        return convertHistogramIdsToText(responses);
+    }
+
+    private void incrementHistogram(Map<String, Map<Boolean, AtomicInteger>> responses, BooleanAnswer answer) {
+        Map<Boolean, AtomicInteger> histogram = getHistogram(responses, answer.getQuestionId());
+        histogram.get(Boolean.valueOf(answer.getValue())).getAndIncrement();
+    }
+
+    private Map<String, Map<Boolean, AtomicInteger>> convertHistogramIdsToText(Map<String, Map<Boolean, AtomicInteger>> responses) {
+        Map<String, Map<Boolean, AtomicInteger>> textResponses = new HashMap<>();
+        responses.keySet().stream().forEach(id -> textResponses.put(controller.find(id).getText(), responses.get(id)));
+        return textResponses;
+    }
+
+    private Map<Boolean, AtomicInteger> getHistogram(Map<Integer, Map<Boolean, AtomicInteger>> responses, int id ) {
+        Map<Boolean, AtomicInteger> histogram = null;
+        if(responses.containsKey(id))
+            histogram = responses.get(id);
+        else {
+            histogram = createNewHistogram();
+            responses.put(id, histogram);
+        }
+
+        return histogram;
+    }
+
+    private Map<Boolean, AtomicInteger> createNewHistogram() {
+        Map<Boolean, AtomicInteger> histogram;
+        histogram = new HashMap<>();
+        histogram.put(Boolean.FALSE, new AtomicInteger(0));
+        histogram.put(Boolean.TRUE, new AtomicInteger(0));
+        return histogram;
+    }
+
+}
